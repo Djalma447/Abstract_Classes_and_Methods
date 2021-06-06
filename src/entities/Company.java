9f9b542000b6b4850685aad60e2ae38e10c1d35f
Djalma <djalma.ant@gmail.com>
2021-06-06 01:24:38 -0300

    Classes and main program done

---
+++ src/entities/Company.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Company extends TaxPayer {
+
+	private Integer numberEmployees;
+	
+	public Company() {
+		super();
+	}
+	
+	public Company(String name, Double anualIncome, Integer numberEmployees) {
+		super(name, anualIncome);
+		this.numberEmployees = numberEmployees;
+	}
+
+	public Integer getNumberEmployees() {
+		return numberEmployees;
+	}
+
+	public void setNumberEmployees(Integer numberEmployees) {
+		this.numberEmployees = numberEmployees;
+	}
+
+	@Override
+	public double tax() {
+		if (numberEmployees <= 10) {
+			return anualIncome * 0.16;
+		}
+		else {
+			return anualIncome * 0.14;
+		}
+	}
+
+}
---
+++ src/entities/TaxPayer.java
@@ -0,0 +1,39 @@
+package entities;
+
+public abstract class TaxPayer {
+
+	private String name;
+	protected Double anualIncome;
+	
+	public TaxPayer() {
+	}
+
+	public TaxPayer(String name, Double anualIncome) {
+		super();
+		this.name = name;
+		this.anualIncome = anualIncome;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getanualIncome() {
+		return anualIncome;
+	}
+
+	public void setanualIncome(Double anualIncome) {
+		this.anualIncome = anualIncome;
+	}
+	
+	public abstract double tax();
+	
+	@Override
+	public String toString() {
+		return name + ": $ " + String.format("%.2f", tax());
+	}
+}
